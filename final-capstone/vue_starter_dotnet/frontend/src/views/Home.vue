89d86c1a63f75a057b60449c5f396643b22280a9
Glenn Weber <glennweber099@gmail.com>
2019-12-10 17:51:04 -0500

    Began to work with the frontend of uploading

--- final-capstone/vue_starter_dotnet/frontend/src/views/Home.vue
+++ final-capstone/vue_starter_dotnet/frontend/src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
+     <router-link :to="{ name: 'upload' }">Upload a Photo</router-link>
   </div>
 </template>
 
