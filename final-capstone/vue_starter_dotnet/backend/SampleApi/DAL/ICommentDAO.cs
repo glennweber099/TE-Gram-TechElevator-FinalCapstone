dc69402ff29a75c35589032b8551a0d6260a9530
Joanna Cregan <joannamcregan@gmail.com>
2019-12-12 09:41:47 -0500

    merge conflicts here we come

---
+++ final-capstone/vue_starter_dotnet/backend/SampleApi/DAL/ICommentDAO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApi.DAL
+{
+    /// <summary>
+    /// An interface associated with the Comments data
+    /// </summary>
+    public interface ICommentDAO
+    {
+        void GetAllcommentsByPhotoId(int photoId);
+    }
+}
---
+++ final-capstone/vue_starter_dotnet/backend/SampleApi/Models/Comment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleApi.Models
+{
+    /// <summary>
+    /// Models a comment
+    /// </summary>
+    public class Comment
+    {
+        /// <summary>
+        /// The actual comment that a site user has inputted
+        /// </summary>
+        public string CommentString { get; set; }
+        /// <summary>
+        /// The comment's id in the database
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// The id of the photo the comment is associated with
+        /// </summary>
+        public int PhotoId { get; set; }
+        /// <summary>
+        /// The id of the user who left the comment
+        /// </summary>
+        public int CommenterId { get; set; }
+        /// <summary>
+        /// The date and time when the comment was left
+        /// </summary>
+        public DateTime DateCommented { get; set; }
+    }
+}
